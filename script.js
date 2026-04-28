d0abf64234d8616c7364139fb16a39e271cec619
ajay333X <75782512+ajay333X@users.noreply.github.com>
2026-04-28 14:46:03 +0530

    Add floating particles animation to canvas

--- script.js
+++ script.js
@@ -54,3 +54,66 @@ window.onclick = function(e) {
     if (e.target == signInModal) closeSignInModal();
     if (e.target == getStartedModal) closeGetStartedModal();
 }
+// --- FLOATING PARTICLES LOGIC ---
+const canvas = document.getElementById('particleCanvas');
+const ctx = canvas.getContext('2d');
+
+let particles = [];
+const particleCount = 40; 
+
+function initCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+
+class Particle {
+    constructor() {
+        this.reset();
+    }
+    reset() {
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;
+        this.size = Math.random() * 2 + 0.5;
+        this.speedX = (Math.random() - 0.5) * 0.4;
+        this.speedY = (Math.random() - 0.5) * 0.4;
+        this.opacity = Math.random() * 0.5;
+    }
+    update() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+        if (this.x > canvas.width || this.x < 0 || this.y > canvas.height || this.y < 0) {
+            this.reset();
+        }
+    }
+    draw() {
+        ctx.fillStyle = `rgba(167, 139, 250, ${this.opacity})`; 
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        ctx.fill();
+    }
+}
+
+function createParticles() {
+    particles = [];
+    for (let i = 0; i < particleCount; i++) {
+        particles.push(new Particle());
+    }
+}
+
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    particles.forEach(p => {
+        p.update();
+        p.draw();
+    });
+    requestAnimationFrame(animate);
+}
+
+window.addEventListener('resize', () => {
+    initCanvas();
+    createParticles();
+});
+
+initCanvas();
+createParticles();
+animate();
