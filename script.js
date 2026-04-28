b52320a7b84f372e4de117b3715b8f56a56d663c
ajay333X <75782512+ajay333X@users.noreply.github.com>
2026-04-28 11:13:46 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// Simple interaction for buttons
+document.querySelectorAll('button').forEach(button => {
+    button.addEventListener('click', () => {
+        console.log("Button clicked!");
+    });
+});
